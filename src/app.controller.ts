f098eab6ec28f6986681cdb11584e557724d5884
minwoo-3 <s26002@gsm.hs.kr>
2026-07-21 00:29:52 +0900

    refactor: 오류 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,18 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHome() {
+    return 'Home Page';
+  }
+
+
+  @Get('post')
+  getPost(){
+    return 'Post Page';
+  }
+
+  @Get('user')
+  getUser(){
+    return 'User Page';
   }
 }
